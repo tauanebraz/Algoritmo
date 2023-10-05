74c2eb0963e08ed845c5be7a63e5f01461afcacd
Tauane braz <tauanebs02@gmail.com>
2023-10-05 17:52:17 -0300

    Alteração de codigo

---
+++ semestre.py
@@ -0,0 +1,20 @@
+# TRANFORMANDO PORTOGOL EM PYTHON 
+#   ==== EXERCICIO 15 ====
+
+nome = input("informe seu nome:")
+np = float(input("informe sua primeira nota?"))
+ns = float(input("informe sua segunda nota?"))
+
+media = (np + ns )/2
+
+print("Aluno(a):", nome)
+
+if media>6:
+    print("Aprovado")
+if media<5 :
+        print("Reprovado")
+    
+
+
+    
+        
